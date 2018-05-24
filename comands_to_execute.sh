55d094c5d2c89b00e094bec5a087f37441e60d1d
ryujycool <38003211+ryujycool@users.noreply.github.com>
2018-05-24 17:35:04 +0200

    Update comands_to_execute.sh

--- comands_to_execute.sh
+++ comands_to_execute.sh
@@ -1,1 +1,5 @@
 
+echo 1 > /proc/sys/net/ipv4/ip_forward
+iptables -t nat -A POSTROUTING -o eth3 -j MASQUERADE
+iptables -A FORWARD -i eth3 -o eth4 -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -i eth4 -o eth3 -j ACCEPT
